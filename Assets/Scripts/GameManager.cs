632d9af1e46c0f6fd3d6de50e8c95d5ff88aebc5
Keven Boily <kboily@hibernum.com>
2017-01-21 00:21:49 -0500

    Added UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,24 +4,73 @@ using UnityEngine;
 
 public class GameManager : BaseSingleton<GameManager> 
 {
+    public GameUI GameUI;
+
     public List<GameObject> Lanes;
     public PlayerCharacter PlayerPrefab;
     public float PlayerOffset = -2.25f;
-    public string PlayerName { get; set; }
-    public long Score = 0;
+
+    public string PlayerName;
+
+    private int _level = 0;
+    public int Level
+    {
+        get 
+        {
+            return _level;
+        }
+        set 
+        {
+            _level = value;
+            GameUI.LevelText.text = "Level " + _level.ToString();
+        }
+    }
+
+    private long _score = 0;
+    public long Score
+    {
+        get 
+        {
+            return _score;
+        }
+        set 
+        {
+            _score = value;
+            GameUI.ScoreText.text = _score.ToString();
+        }
+    }
+
+    private int _multiplier = 0;
+    public int Multiplier
+    {
+        get 
+        {
+            return _multiplier;
+        }
+        set 
+        {
+            _multiplier = value;
+            GameUI.MultiplierText.text = "X" + _multiplier.ToString();
+        }
+    }
+
     public PlayerCharacter PlayerCharacter { get; private set; }
     public List<HighScoreTool.HighScoreEntry> currentHighScores = HighScoreTool.FetchHighScores();
     public int CurrentLane { get; private set; }
 
     void Start()
     {
-        CleanUp ();
+        Reset ();
     }
 
     private void StartGame()
     {
-        CleanUp ();
+        Reset ();
+
         Score = 0;
+        Multiplier = 1;
+        Level = 1;
+
         PlayerName = "MTL";
         PlayerCharacter = Instantiate (PlayerPrefab);
         PlayerCharacter.transform.localPosition = new Vector3(PlayerOffset, 0, 0);
@@ -30,18 +79,18 @@ public class GameManager : BaseSingleton<GameManager>
     private void SendHighScore()
     {
 
-        currentHighScores = HighScoreTool.SendHighScore(PlayerName, Score);
+        currentHighScores = HighScoreTool.SendHighScore(PlayerName, _score);
     }
     private void GameOver()
     {
         /*  Score = 0;
         PlayerName = "MTL";*/
-        Score = 5;
+        _score = 5;
         PlayerName = "MTL";
 
 
         currentHighScores = HighScoreTool.FetchHighScores();
-        if (Score > long.Parse(currentHighScores[currentHighScores.Count - 1].score))
+        if (_score > long.Parse(currentHighScores[currentHighScores.Count - 1].score))
         {
 
             Debug.Log("NEW HIGH SCORE!");
@@ -68,13 +117,20 @@ public class GameManager : BaseSingleton<GameManager>
         }
     }
 
-    private void CleanUp()
+    private void Reset()
     {
         if(PlayerCharacter != null)
         {
             DestroyImmediate (PlayerCharacter.gameObject);
             PlayerCharacter = null;
         }
+
+        _level = 0;
+        _multiplier = 0;
+        _score = 0;
+
+        GameUI.Reset ();
+
         CurrentLane = -1;
     }
 
