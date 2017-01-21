60017deede8b18c04d83489f43832e1bb0779ca2
Elie Rodrigue <elie.rodrigue@nubik.ca>
2017-01-21 10:04:25 -0500

    Title screen

---
+++ Assets/TitleScreenHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScreenHandler : MonoBehaviour {
+
+    public GameManager theUi;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.anyKeyDown)
+        {
+            DestroyImmediate(this.gameObject);
+        }	
+	}
+}
