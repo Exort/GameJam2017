abdd874702c62f53881937a04a9110be090dcb92
Michael Larouche <michael.larouche@gmail.com>
2017-01-21 10:15:30 -0500

    Add blink to title screen

--- Assets/TitleScreenHandler.cs
+++ Assets/TitleScreenHandler.cs
@@ -1,13 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TitleScreenHandler : MonoBehaviour {
 
     public GameManager theUi;
+
+    public float BlinkTime = 0.4f;
+    public Text AnyKeyComponent;
+
+    private float blinkTimer;
+
 	// Use this for initialization
 	void Start () {
-		
+        blinkTimer = 0f;
 	}
 	
 	// Update is called once per frame
@@ -15,6 +22,16 @@ public class TitleScreenHandler : MonoBehaviour {
 	    if(Input.anyKeyDown)
         {
             DestroyImmediate(this.gameObject);
-        }	
+        }
+
+        blinkTimer += Time.deltaTime;
+        if (blinkTimer >= BlinkTime)
+        {
+            if (AnyKeyComponent != null)
+            {
+                AnyKeyComponent.enabled = !AnyKeyComponent.enabled;
+                blinkTimer = 0f;
+            }
+        }
 	}
 }
